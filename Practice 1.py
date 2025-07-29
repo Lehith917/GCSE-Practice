00239de8ecfb9c8777a42b7dbd4756581cc78a79
Lehith917 <legacylehith@gmail.com>
2025-07-29 16:51:44 +0100

    Add files via upload

---
+++ Practice 1.py
@@ -0,0 +1,3 @@
+Name1=input("Enter your first name: ")
+Name2=input("Enter your last name: ")
+print(Name1+"'s first name is",Name2)
---
+++ Practice 2.py
@@ -0,0 +1,21 @@
+#Let the length of the rectangle be 50 and height of the rectangle be 80.
+#Calculate and print the perimeter and area of the rectangle
+# length=50
+# height=80
+# print("The area of the rectangle is",length*height)
+# print("The perimeter of the rectangle is",(length*2)+(height*2))
+
+#Ask for total numbers of candies and number of people
+#Calculate candies per person and leftovers
+# candies=int(input("Enter the total number of candies: "))
+# people=int(input("Enter the total number of people: "))
+# print("The amount of candies per person is",candies//people) 
+# print("The amount of leftovers is",candies%people)
+
+a="Pear"
+b="Apple"
+c=18
+print(a+str(c))
+print(a*2)
+
+
