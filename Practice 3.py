a31973a2a818d32fc684dd200f57addad39c9225
Lehith917 <legacylehith@gmail.com>
2025-08-02 16:42:50 +0100

    Add files via upload

---
+++ Practice 3.py
@@ -0,0 +1,13 @@
+# num1=int(input("Enter a number: "))
+# num2=int(input("Enter a number: "))
+# sum=num1+num2
+# difference=num1-num2
+# print("The sum of your two numbers is "+str(num1)+" + "+str(num2)+" = "+str(sum))
+# print("The difference of your two numbers is "+str(num1)+" - "+str(num2)+" = "+str(difference))
+
+# num1=float(input("Enter a number: "))
+# num2=float(input("Enter a number: "))
+# perimeter=(num1*2)+(num2*2)
+# area=num1*num2
+# print("The perimeter of these numbers is",perimeter)
+# print("The area of these numbers is",area)
