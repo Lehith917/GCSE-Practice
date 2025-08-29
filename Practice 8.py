81ae0122b66922a1a6aabe309a0f388a3b44f264
Lehith917 <legacylehith@gmail.com>
2025-08-29 18:55:41 +0100

    Add files via upload

---
+++ Practice 8.py
@@ -0,0 +1,24 @@
+# list=[[1,2],[3,4],[-2,6]]
+# list[1][1]=9
+
+# print(list[2][1])
+# k=0
+# for i in list:
+#     for j in i:
+#         if j>k:
+#             print("The current smallest number is",k)  
+#         else:
+#             k=j  
+# import random
+# pnumber=int(input("Enter the amount of players: "))
+# rounds=5
+# l=[]
+# for i in range(pnumber):
+#     for j in range(rounds):
+#         l[i][j]=random.randint(1,10)
+# #calculate every players score and declare who won every 5 rounds
+
+list=[1,2,3,4,5]
+for i in list:
+    print(list[i])
+
